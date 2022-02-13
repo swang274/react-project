14a9518fb10917c24d51d17db3d76d94becd0789
Shunfeng Wang <sf.designspace@gmail.com>
2022-02-13 10:47:56 -0600

    first upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo possimus
+        hic provident laboriosam assumenda, sequi quos quibusdam cupiditate
+        expedita. Repellat delectus ut modi sapiente dolorem at nihil veniam
+        odit dolores?
+      </p>
+    </section>
+  );
+};
+
+export default About;
